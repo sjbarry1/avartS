7d3c457e4c5e9bbc9590df2b9e6803692613110c
Matthew Oujiri <mkoujiri@mtu.edu>
2024-03-17 16:54:41 -0400

    Added Yearly and Weekly Graphs
    wow that was tough

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,9 @@
 import os
 
-from flask import Flask,session,redirect,request,render_template
+from flask import Flask,session,redirect,request,render_template,jsonify,after_this_request
 import requests
+import datetime
+import numpy as np
 # from flask_session import Session
 
 def create_app(test_config=None):
@@ -16,6 +18,7 @@ def create_app(test_config=None):
         return render_template("login.html")
     @app.route('/login.html',methods=["GET"])
     def login():
+        print(session.get('access_token'))
         if session.get('access_token') == None:
             return redirect('http://www.strava.com/oauth/authorize?client_id=117096&response_type=code&redirect_uri=http://127.0.0.1:5000/hello&approval_prompt=force&scope=read_all,activity:read_all')
         else:
@@ -41,9 +44,10 @@ def create_app(test_config=None):
             access_token = session['access_token']
             get_req = requests.get(f"https://www.strava.com/api/v3/activities?access_token={access_token}")
             json_data = get_req.json()
-            page_info = ""
-            for i in range(len(json_data)):
-                page_info+=json_data[i]['name']+'<br>'
+            #page_info = ""
+            #for i in range(len(json_data)):
+            #    page_info+=json_data[i]['name']+'<br>'
+            return render_template("main.html")
         except KeyError:
             return redirect("/")
         return page_info
@@ -52,4 +56,100 @@ def create_app(test_config=None):
         session.pop('access_token',default=None)
         return "logged out",{"Refresh": "1; url=/"}
 
+
+    @app.route('/me/weekly_data',methods=["GET"])
+    def weekly_data():
+        @after_this_request
+        def add_header(response):
+            # response.headers['Access-Control-Allow-Origin'] = '*'
+            response.headers.add('Access-Control-Allow-Origin', '*')
+            return response
+        # get access token
+        access_token = session['access_token']
+
+        # find weekly range in EPOCH
+        start_date = int(request.args.get('start_date'))/1000
+        week_time = 604800
+        end_date = start_date+week_time
+        # get data from Strava API
+        strava_request = requests.get(f"https://www.strava.com/api/v3/activities?before={end_date}&after={start_date}&access_token={access_token}")
+        json_data = strava_request.json()
+        # populate empty lists
+        result = {"data":[0]*7}
+        # time for a day
+        delta = 86400
+        for i in range(len(json_data)):
+            activity_date = json_data[i]['start_date']
+            activity_epoch = datetime.datetime(int(activity_date[0:4]),int(activity_date[5:7]),int(activity_date[8:10])).strftime("%s")
+            diff = int(activity_epoch) - start_date
+            # distance in km
+            distance = json_data[i]['distance']/1000
+            # store
+            result['data'][int(diff//delta)]+=distance
+        for i in range(7):
+            # round to %.2f
+            result['data'][i]="{:.2f}".format(result['data'][i])
+
+        return jsonify(result)
+
+    @app.route('/me/yearly_data',methods=["GET"])
+    def yearly_data():
+        @after_this_request
+        def add_header(response):
+            # response.headers['Access-Control-Allow-Origin'] = '*'
+            response.headers.add('Access-Control-Allow-Origin', '*')
+            return response
+        # get access token
+        access_token = session['access_token']
+
+        # find weekly range in EPOCH
+        start_year = 2020
+        # TODO calculate this
+        current_year = 2024
+        start_date = 1577836800
+        year_time = 31556926
+        json_data = []
+        for index,year in enumerate(range(start_year,current_year)):
+            end_date = start_date+year_time*(index+1)
+
+            json_data.append([])
+            # get data from Strava API
+            for i in range(1,6):
+                strava_request = requests.get(f"https://www.strava.com/api/v3/activities?before={end_date+year_time*index}&after={start_date+year_time*index}&access_token={access_token}&per_page=200&page={i}")
+                json_data[index] += strava_request.json()
+
+        # populate empty lists
+        blank_data = []
+        for i in range(current_year-start_year):
+            blank_data.append([0]*365)
+        result = {"data":blank_data}
+        print(result)
+        # time for a day
+        delta = 86400
+        print(len(blank_data))
+        for index in range(len(blank_data)):
+            for i in range(len(json_data[index])):
+                activity_date = json_data[index][i]['start_date']
+                activity_epoch = datetime.datetime(int(activity_date[0:4]),int(activity_date[5:7]),int(activity_date[8:10])).strftime("%s")
+                diff = int(activity_epoch) - start_date+year_time*index
+                # distance in km
+                distance = json_data[index][i]['distance']/1000
+                # store
+                print('day of year',int(diff//delta%365))
+                result['data'][index][int(diff//delta%365)]+=distance
+            result['data'][index] = list(np.cumsum(result['data'][index]))
+            for i in range(365):
+                # round to %.2f
+                result['data'][index][i]="{:.2f}".format(result['data'][index][i])
+
+        print(result)
+        return jsonify(result)
+
+
+
+
+        #response = {'hello':123,'goodbye':12345}
+        #return jsonify(response)
+
     return app
+
